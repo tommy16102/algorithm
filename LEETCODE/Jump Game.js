b6ad2fea1c3af976ae5ff9042349a8959188988d
서혜민 <75344562+tommy16102@users.noreply.github.com>
2023-01-30 10:05:25 +0900

    Jump Game

---
+++ LEETCODE/Jump Game.js
@@ -0,0 +1,10 @@
+var canJump = function(nums) {
+    let max = nums[0];
+  
+    for (let i = 0; i < nums.length; i++) {
+        if (i > max) return false
+        max = Math.max(max, i + nums[i]);
+    }
+  
+    return true;
+};
