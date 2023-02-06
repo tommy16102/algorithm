ed9639806b5f45e6bd8719db0cb32a97ef80fb3d
서혜민 <75344562+tommy16102@users.noreply.github.com>
2023-02-06 10:05:34 +0900

    Valid Palindrome

---
+++ LEETCODE/Valid Palindrome.js
@@ -0,0 +1,5 @@
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    for (let i = 0, j = s.length - 1; i <= j; i++, j--) if (s[i] !== s[j]) return false;
+    return true;
+};
